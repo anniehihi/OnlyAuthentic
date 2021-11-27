e73925f7c948dfd57cbc0d7ff0f7e7c584565b4d
datmax01 <datpham10082001@gmail.com>
2021-11-27 13:21:51 +0700

    Staff done 2

--- Admin/manage-staff.php
+++ Admin/manage-staff.php
@@ -1,74 +1,129 @@
 <?php include('./partials/header.php'); ?>
+
+ <?php
+                if(isset($_SESSION['upload'])){
+                    echo $_SESSION['upload'];
+                    unset($_SESSION['upload']);
+                }
+                if(isset($_SESSION['add'])){
+                    echo $_SESSION['add'];
+                    unset($_SESSION['add']);
+                }
+                if(isset($_SESSION['no-category-found'])){
+                    echo $_SESSION['no-category-found'];
+                    unset($_SESSION['no-category-found']);
+                }
+
+                if(isset($_SESSION['update'])){
+                    echo $_SESSION['update'];
+                    unset($_SESSION['update']);
+                }
+
+                
+                if(isset($_SESSION['upload'])){
+                    echo $_SESSION['upload'];
+                    unset($_SESSION['upload']);
+                }
+
+                if(isset($_SESSION['failed-remove'])){
+                    echo $_SESSION['failed-remove'];
+                    unset($_SESSION['failed-remove']);
+                }
+
+                if(isset($_SESSION['delete'])){
+                    echo $_SESSION['delete'];
+                    unset($_SESSION['delete']);
+                }
+
+                if(isset($_SESSION['remove'])){
+                    echo $_SESSION['remove'];
+                    unset($_SESSION['remove']);
+                }
+            ?>
             <!-- START PAGE CONTENT-->
+
             <div class="ibox">
                 <div class="ibox-head">
-                    <div class="ibox-title">Responsive Table</div>
+                    <a href="add-staff.php"><button class="btn btn-default btn-xs" data-toggle="tooltip" data-original-title="Add"><i class="fa fa-plus font-14"></i></button></a>
                 </div>
                 <div class="ibox-body">
                     <div class="table-responsive">
                         <table class="table">
                             <thead>
                                 <tr>
-                                    <th width="50px"></th>
-                                    <th>Product</th>
-                                    <th>Price</th>
-                                    <th>Data</th>
-                                    <th>Last Name</th>
-                                    <th>Actions</th>
+                                    <th>S.N</th>
+                                    <th>First_Name</th>
+                                    <th>Last_Name</th>
+                                    <th>Age</th>
+                                    <th>Address</th>
+                                    <th>Email</th>
+                                    <th>Contact</th>
+                                    <th>Image</th>
                                 </tr>
                             </thead>
                             <tbody>
-                                <tr>
-                                    <td>
-                                        <label class="ui-checkbox">
-                                            <input type="checkbox">
-                                            <span class="input-span"></span>
-                                        </label>
-                                    </td>
-                                    <td>iphone case</td>
-                                    <td>$1200</td>
-                                    <td>33%</td>
-                                    <td>02/08/2017</td>
-                                    <td>
-                                        <a href="add-staff.php"><button class="btn btn-default btn-xs" data-toggle="tooltip" data-original-title="Add"><i class="fa fa-plus font-14"></i></button></a>
-                                        <button class="btn btn-default btn-xs m-r-5" data-toggle="tooltip" data-original-title="Edit"><i class="fa fa-pencil font-14"></i></button>
-                                        <button class="btn btn-default btn-xs" data-toggle="tooltip" data-original-title="Delete"><i class="fa fa-trash font-14"></i></button>
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td>
-                                        <label class="ui-checkbox">
-                                            <input type="checkbox">
-                                            <span class="input-span"></span>
-                                        </label>
-                                    </td>
-                                    <td>Car covers</td>
-                                    <td>$3280</td>
-                                    <td>42%</td>
-                                    <td>08/10/2017</td>
-                                    <td>
-                                        <button class="btn btn-default btn-xs" data-toggle="tooltip" data-original-title="Delete"><i class="fa fa-plus font-14"></i></button>
-                                        <button class="btn btn-default btn-xs m-r-5" data-toggle="tooltip" data-original-title="Edit"><i class="fa fa-pencil font-14"></i></button>
-                                        <button class="btn btn-default btn-xs" data-toggle="tooltip" data-original-title="Delete"><i class="fa fa-trash font-14"></i></button>
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td>
-                                        <label class="ui-checkbox">
-                                            <input type="checkbox">
-                                            <span class="input-span"></span>
-                                        </label>
-                                    </td>
-                                    <td>Compressors</td>
-                                    <td>$7400</td>
-                                    <td>56%</td>
-                                    <td>14/11/2017</td>
-                                    <td>
-                                        <button class="btn btn-default btn-xs" data-toggle="tooltip" data-original-title="Delete"><i class="fa fa-plus font-14"></i></button>
-                                        <button class="btn btn-default btn-xs m-r-5" data-toggle="tooltip" data-original-title="Edit"><i class="fa fa-pencil font-14"></i></button>
-                                        <button class="btn btn-default btn-xs" data-toggle="tooltip" data-original-title="Delete"><i class="fa fa-trash font-14"></i></button>
-                                    </td>
-                                </tr>
+                                <?php
+                                    // câu truy vấn để lấy dữ liệu 
+                                    $sql = "SELECT * FROM tbl_staff"; 
+
+                                    // thực thi câu truy vấn
+                                    $res = mysqli_query($conn, $sql); 
+
+                                    // kiểm tra xem có dữ liệu hay không 
+                                    $count = mysqli_num_rows($res); 
+
+                                    $sn = 1; 
+
+                                    if($count > 0){
+                                        while($row = mysqli_fetch_assoc($res)){
+                                            $id = $row['id']; 
+                                            $first_name = $row['first_name'];
+                                            $last_name = $row['last_name']; 
+                                            $age = $row['age']; 
+                                            $address = $row['address']; 
+                                            $email = $row['email'];
+                                            $contact = $row['contact'];
+                                            $image_name = $row['image_name'];
+
+                                            ?>
+                                                <tr>
+                                                    <td><?php echo $sn++; ?></td>
+                                                    <td><?php echo $first_name; ?></td>
+                                                    <td><?php echo $last_name; ?></td>
+                                                    <td><?php echo $age; ?></td>
+                                                    <td><?php echo $address; ?></td>
+                                                    <td><?php echo $email; ?></td>
+                                                    <td><?php echo $contact; ?></td>
+                                                    <td>
+                                                        <?php
+                                                            if($image_name != ""){
+                                                                ?>
+                                                                    <img src="<?php echo SITEURL; ?>img/staff/<?php echo $image_name; ?>"
+                                                                    width = "100px">
+                                                                <?php
+                                                            }else{
+                                                                echo "<p class='text-success'>Image Not Added</p>";
+                                                            }
+                                                        ?>
+                                                    </td>
+                                            
+                                                    <td>
+                                                        <a href="<?php echo SITEURL; ?>admin/update-staff.php?id=<?php echo $id;?>"><button class="btn btn-default btn-xs m-r-5" data-toggle="tooltip" data-original-title="Update"><i class="fa fa-pencil font-14"></i></button></a> 
+                                                        <a href="<?php echo SITEURL; ?>admin/delete-staff.php?id=<?php echo $id;?>&image_name=<?php echo $image_name; ?>"><button class="btn btn-default btn-xs" data-toggle="tooltip" data-original-title="Delete"><i class="fa fa-trash font-14"></i></button></a> 
+                                                    </td>
+                                                </tr>
+                                            <?php
+                                        }
+                                    }else{
+                                        ?>
+                                            <tr>
+                                                <td>
+                                                    <div class='text-success'>Image Not Added</div>
+                                                </td>
+                                            </tr>
+                                        <?php
+                                    }
+                                ?>
                             </tbody>
                         </table>
                     </div>
