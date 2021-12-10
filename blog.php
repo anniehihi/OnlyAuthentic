d9b5e70355975e2db353873a0e7823652a9e09f3
Tue <tue1252001@gmail.com>
2021-12-10 13:27:43 +0700

    sua blog

--- blog.php
+++ blog.php
@@ -4,7 +4,7 @@
       <div class="container">
         <div class="row no-gutters slider-text align-items-center justify-content-center">
           <div class="col-md-9 ftco-animate text-center">
-          	<p class="breadcrumbs"><span class="mr-2"><a href="index.php">Home</a></span> <span>Blog</span></p>
+          	<p class="breadcrumbs"><span class="mr-2"><a href="index.php">TRANG CHỦ</a></span> <span>Blog</span></p>
             <h1 class="mb-0 bread">Blog</h1>
           </div>
         </div>
@@ -22,13 +22,13 @@
 		              </a>
 		              <div class="text d-block pl-md-4">
 		              	<div class="meta mb-3">
-		                  <div><a href="#">April 9, 2019</a></div>
+		                  <div><a href="#">NGÀY 9 THÁNG 4 NĂM 2021</a></div>
 		                  <div><a href="#">Admin</a></div>
 		                  <div><a href="#" class="meta-chat"><span class="icon-chat"></span> 3</a></div>
 		                </div>
-		                <h3 class="heading"><a href="#">Even the all-powerful Pointing has no control about the blind texts</a></h3>
-		                <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
-		                <p><a href="blog-single.html" class="btn btn-primary py-2 px-3">Read more</a></p>
+		                <h3 class="heading"><a href="#">Mua Sắm online đang là xu hướng</a></h3>
+		                <p>Giúp người dùng dễ dàng thuận tiện mua bán tại nhà trong thời điểm dịch Covid này.</p>
+		                <p><a href="blog-single.html" class="btn btn-primary py-2 px-3">Đọc thêm</a></p>
 		              </div>
 		            </div>
 		          </div>
@@ -38,13 +38,13 @@
 		              </a>
 		              <div class="text d-block pl-md-4">
 		              	<div class="meta mb-3">
-		                  <div><a href="#">April 9, 2019</a></div>
+		                  <div><a href="#">NGÀY 9 THÁNG 4 NĂM 2021</a></div>
 		                  <div><a href="#">Admin</a></div>
 		                  <div><a href="#" class="meta-chat"><span class="icon-chat"></span> 3</a></div>
 		                </div>
-		                <h3 class="heading"><a href="#">Even the all-powerful Pointing has no control about the blind texts</a></h3>
-		                <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
-		                <p><a href="blog-single.html" class="btn btn-primary py-2 px-3">Read more</a></p>
+		                <h3 class="heading"><a href="#">Chia sẻ của cô gái có làn da đẹp</a></h3>
+		                <p>Việc chăm sóc về ngoại hình bản thân không chỉ còn quan trọng với phái nữ nữa mà cả đàn ông bây giờ họ cũng rất quan tâm về vẻ ngoài.</p>
+		                <p><a href="blog-single.html" class="btn btn-primary py-2 px-3">Đọc thêm</a></p>
 		              </div>
 		            </div>
 		          </div>
@@ -54,13 +54,13 @@
 		              </a>
 		              <div class="text d-block pl-md-4">
 		              	<div class="meta mb-3">
-		                  <div><a href="#">April 9, 2019</a></div>
+		                  <div><a href="#">NGÀY 9 THÁNG 4 NĂM 2021</a></div>
 		                  <div><a href="#">Admin</a></div>
 		                  <div><a href="#" class="meta-chat"><span class="icon-chat"></span> 3</a></div>
 		                </div>
-		                <h3 class="heading"><a href="#">Even the all-powerful Pointing has no control about the blind texts</a></h3>
-		                <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
-		                <p><a href="blog-single.html" class="btn btn-primary py-2 px-3">Read more</a></p>
+		                <h3 class="heading"><a href="#">Cô tiểu thư của ca sỹ nổi tiếng với gu ăn mặc độc lạ thời trang</a></h3>
+		                <p>Chia sẻ của tiểu thư sinh năm 2005 về phong cách ăn mặc của mình ảnh rất nhiều đến từ mẹ cô và dòng nhạc rock.</p>
+		                <p><a href="blog-single.html" class="btn btn-primary py-2 px-3">Đọc thêm</a></p>
 		              </div>
 		            </div>
 		          </div>
@@ -70,13 +70,13 @@
 		              </a>
 		              <div class="text d-block pl-md-4">
 		              	<div class="meta mb-3">
-		                  <div><a href="#">April 9, 2019</a></div>
+		                  <div><a href="#">NGÀY 9 THÁNG 4 NĂM 2021</a></div>
 		                  <div><a href="#">Admin</a></div>
 		                  <div><a href="#" class="meta-chat"><span class="icon-chat"></span> 3</a></div>
 		                </div>
-		                <h3 class="heading"><a href="#">Even the all-powerful Pointing has no control about the blind texts</a></h3>
-		                <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
-		                <p><a href="blog-single.html" class="btn btn-primary py-2 px-3">Read more</a></p>
+		                <h3 class="heading"><a href="#">Những bức ảnh đẹp của nữ diva đang mang thai</a></h3>
+		                <p>Nữ diva sinh săm 1986 dù cô đã ở độ tuổi ngoài 30 nhưng cách ăn mặc của cô vẫn không bao giờ hết hot.</p>
+		                <p><a href="blog-single.html" class="btn btn-primary py-2 px-3">Đọc thêm</a></p>
 		              </div>
 		            </div>
 		          </div>
@@ -86,13 +86,13 @@
 		              </a>
 		              <div class="text d-block pl-md-4">
 		              	<div class="meta mb-3">
-		                  <div><a href="#">April 9, 2019</a></div>
+		                  <div><a href="#">NGÀY 9 THÁNG 4 NĂM 2021</a></div>
 		                  <div><a href="#">Admin</a></div>
 		                  <div><a href="#" class="meta-chat"><span class="icon-chat"></span> 3</a></div>
 		                </div>
-		                <h3 class="heading"><a href="#">Even the all-powerful Pointing has no control about the blind texts</a></h3>
-		                <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
-		                <p><a href="blog-single.html" class="btn btn-primary py-2 px-3">Read more</a></p>
+		                <h3 class="heading"><a href="#">Minishop trang mua sắm đứng trong top những cửa hàng bán chạy nhất thế giới</a></h3>
+		                <p>Thành lập năm 2010 đến nay Minishop đã xây dựng được tên tuổi cũng như thương hiệu của bản thân.</p>
+		                <p><a href="blog-single.html" class="btn btn-primary py-2 px-3">Đọc thêm</a></p>
 		              </div>
 		            </div>
 		          </div>
@@ -118,30 +118,30 @@
               <form action="#" class="search-form">
                 <div class="form-group">
                   <span class="icon ion-ios-search"></span>
-                  <input type="text" class="form-control" placeholder="Type a keyword and hit enter">
+                  <input type="text" class="form-control" placeholder="Tìm Kiếm">
                 </div>
               </form>
             </div>
             <div class="sidebar-box ftco-animate">
-            	<h3 class="heading">Categories</h3>
+            	<h3 class="heading">THỂ LOẠI</h3>
               <ul class="categories">
-                <li><a href="#">Shoes <span>(12)</span></a></li>
-                <li><a href="#">Men's Shoes <span>(22)</span></a></li>
-                <li><a href="#">Women's <span>(37)</span></a></li>
-                <li><a href="#">Accessories <span>(42)</span></a></li>
-                <li><a href="#">Sports <span>(14)</span></a></li>
-                <li><a href="#">Lifestyle <span>(140)</span></a></li>
+                <li><a href="#">Giày <span>(12)</span></a></li>
+                <li><a href="#">Giày Nam <span>(22)</span></a></li>
+                <li><a href="#">Giày Nữ <span>(37)</span></a></li>
+                <li><a href="#">Phụ kiện <span>(42)</span></a></li>
+                <li><a href="#">Thể thao <span>(14)</span></a></li>
+                <li><a href="#">Cách sống <span>(140)</span></a></li>
               </ul>
             </div>
 
             <div class="sidebar-box ftco-animate">
-              <h3 class="heading">Recent Blog</h3>
+              <h3 class="heading">BLOG GẦN ĐÂY</h3>
               <div class="block-21 mb-4 d-flex">
                 <a class="blog-img mr-4" style="background-image: url(images/image_1.jpg);"></a>
                 <div class="text">
-                  <h3 class="heading-1"><a href="#">Even the all-powerful Pointing has no control about the blind texts</a></h3>
+                  <h3 class="heading-1"><a href="#">Mua Sắm online đang là xu hướng</a></h3>
                   <div class="meta">
-                    <div><a href="#"><span class="icon-calendar"></span> April 27, 2019</a></div>
+                    <div><a href="#"><span class="icon-calendar"></span> NGÀY 9 THÁNG 4 NĂM 2021</a></div>
                     <div><a href="#"><span class="icon-person"></span> Admin</a></div>
                     <div><a href="#"><span class="icon-chat"></span> 19</a></div>
                   </div>
@@ -150,9 +150,9 @@
               <div class="block-21 mb-4 d-flex">
                 <a class="blog-img mr-4" style="background-image: url(images/image_2.jpg);"></a>
                 <div class="text">
-                  <h3 class="heading-1"><a href="#">Even the all-powerful Pointing has no control about the blind texts</a></h3>
+                  <h3 class="heading-1"><a href="#">Chia sẻ của cô gái có làn da đẹp</a></h3>
                   <div class="meta">
-                    <div><a href="#"><span class="icon-calendar"></span> April 27, 2019</a></div>
+                    <div><a href="#"><span class="icon-calendar"></span> NGÀY 9 THÁNG 4 NĂM 2021</a></div>
                     <div><a href="#"><span class="icon-person"></span> Admin</a></div>
                     <div><a href="#"><span class="icon-chat"></span> 19</a></div>
                   </div>
@@ -161,9 +161,9 @@
               <div class="block-21 mb-4 d-flex">
                 <a class="blog-img mr-4" style="background-image: url(images/image_3.jpg);"></a>
                 <div class="text">
-                  <h3 class="heading-1"><a href="#">Even the all-powerful Pointing has no control about the blind texts</a></h3>
+                  <h3 class="heading-1"><a href="#">Cô tiểu thư của ca sỹ nổi tiếng với gu ăn mặc độc lạ thời trang</a></h3>
                   <div class="meta">
-                    <div><a href="#"><span class="icon-calendar"></span> April 27, 2019</a></div>
+                    <div><a href="#"><span class="icon-calendar"></span> NGÀY 9 THÁNG 4 NĂM 2021</a></div>
                     <div><a href="#"><span class="icon-person"></span> Admin</a></div>
                     <div><a href="#"><span class="icon-chat"></span> 19</a></div>
                   </div>
@@ -175,19 +175,19 @@
               <h3 class="heading">Tag Cloud</h3>
               <div class="tagcloud">
                 <a href="#" class="tag-cloud-link">shop</a>
-                <a href="#" class="tag-cloud-link">products</a>
-                <a href="#" class="tag-cloud-link">shirt</a>
-                <a href="#" class="tag-cloud-link">jeans</a>
-                <a href="#" class="tag-cloud-link">shoes</a>
-                <a href="#" class="tag-cloud-link">dress</a>
-                <a href="#" class="tag-cloud-link">coats</a>
-                <a href="#" class="tag-cloud-link">jumpsuits</a>
+                <a href="#" class="tag-cloud-link">mỹ phẩm</a>
+                <a href="#" class="tag-cloud-link">sơ mi</a>
+                <a href="#" class="tag-cloud-link">quần bò</a>
+                <a href="#" class="tag-cloud-link">giày</a>
+                <a href="#" class="tag-cloud-link">đầm</a>
+                <a href="#" class="tag-cloud-link">áo khoác</a>
+                <a href="#" class="tag-cloud-link">áo liền quần</a>
               </div>
             </div>
 
             <div class="sidebar-box ftco-animate">
-              <h3 class="heading">Paragraph</h3>
-              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ducimus itaque, autem necessitatibus voluptate quod mollitia delectus aut, sunt placeat nam vero culpa sapiente consectetur similique, inventore eos fugit cupiditate numquam!</p>
+              <h3 class="heading">ĐOẠN VĂN</h3>
+              <p>Bản thân nỗi đau là rất nhiều nỗi đau Do đó, chúng tôi nghĩ rằng những thú vui của nhu cầu thiết yếu, hoặc sự mềm mại của những lựa chọn, là có thể chấp nhận được!</p>
             </div>
           </div>
 
@@ -199,8 +199,8 @@
     	<div class="container">
     		<div class="row justify-content-center">
     			<div class="col-md-8 heading-section text-center mb-4 ftco-animate">
-            <h2 class="mb-4">Follow Us On Instagram</h2>
-            <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in</p>
+            <h2 class="mb-4">Theo dõi chúng tôi trên Instagram</h2>
+            <p>Tuổi trẻ ta hãy sống trọn với cái gọi là đam mê.</p>
           </div>
     		</div>
     	</div>
